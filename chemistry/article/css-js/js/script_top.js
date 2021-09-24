ff97a48db932b428337f89cd67b320615a30d05a
inncreatblog <90472657+inncreatblog@users.noreply.github.com>
2021-09-24 13:46:15 -0500

    Add files via upload

---
+++ chemistry/article/css-js/js/script_top.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
+
+
+
+$(document).ready(function(){
+
+	$('.label').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+
+
+});
+
+$(document).ready(function(){
+
+	$('.ftr-ico').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+
+
+});
